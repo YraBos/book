acc9d781de52362931f28e834eed1c76f01f69b5
YraBos <yrabos64@gmail.com>
2024-07-19 19:00:53 +0300

    new

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,51 +7,55 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+module.exports = {
+  title: 'Книга lsFusion',
+  tagline: 'lsFusion',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sbook/',
 
+  // Set the production url of your site here
+  url: 'https://yrabos.github.io',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'yrabos', // Usually your GitHub org/user name.
+  projectName: 'sbook', // Usually your repo name.
+  deploymentBranch: 'main',
+  trailingSlash: true,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
+
+  staticDirectories: ['static'],
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ru',
+    locales: ['ru'],
   },
 
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/YraBos/book/edit/main',
+          editLocalizedFiles: true,
+          routeBasePath: '/',
+          showLastUpdateTime: true,
         },
         blog: {
-          showReadingTime: true,
+          showReadingTime: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+//          editUrl:
+//            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -64,78 +68,90 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+//      image: 'img/docusaurus-social-card.jpg',
+    // Поиск по сайту
+//    algolia: {
+//       appId: 'E1PO5EGBDA',
+//       apiKey: '57774cf7001ec391f7bb1a94e65e77df',
+//       indexName: 'yrabosio',
+//       contextualSearch: true,
+//    },
       navbar: {
-        title: 'My Site',
+        title: 'Книга',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Logo',
           src: 'img/logo.svg',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+//        items: [
+//          {
+//            type: 'docSidebar',
+//            sidebarId: 'tutorialSidebar',
+//            position: 'left',
+//            label: 'Tutorial',
+//          },
+//          {to: '/blog', label: 'Blog', position: 'left'},
+//          {
+//            href: 'https://github.com/yrabos/wms_docs',
+//            label: 'GitHub',
+//            position: 'right',
+//          },
+//          {
+//            type: 'localeDropdown',
+//            position: 'right',
+//          },
+//        ],
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
+//      footer: {
+//        style: 'dark',
+//        links: [
+////          {
+////            title: 'Docs',
+////            items: [
+////              {
+////                label: 'Tutorial',
+////                to: '/docs/intro',
+////              },
+////            ],
+////          },
+//          {
+//            title: 'Community',
+//            items: [
+//              {
+//                label: 'Telegram',
+//                href: 'https://t.me/lsfusion_official',
+//              },
+//              {
+//                label: 'Slack',
+//                href: 'https://join.slack.com/t/lsfusion/shared_invite/zt-1itj7vlmf-9aBIP__nj9iMJglDaecqXg',
+//              },
+//              {
+//                label: 'Stack Overflow',
+//                href: 'https://ru.stackoverflow.com/questions/tagged/lsfusion',
+//              },
+//            ],
+//          },
+////          {
+////            title: 'More',
+////            items: [
+////              {
+////                label: 'Blog',
+////                to: '/blog',
+////              },
+////              {
+////                label: 'GitHub',
+////                href: 'https://github.com/facebook/docusaurus',
+////              },
+////            ],
+////          },
+//        ],
+////        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+//      },
       prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
+        theme: prismThemes.nightOwlLight,
+        darkTheme: prismThemes.vsDark,
+        additionalLanguages: ['bash', 'css', 'java', 'json'],
       },
     }),
 };
 
-export default config;
+
